e655e943ae0561f3b810ad51d00dbacc390d9e48
qhzgit734 <109198690+qhzgit734@users.noreply.github.com>
2022-12-13 13:55:17 +0800

    Add files via upload

---
+++ lib/textshot.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+import io
+import sys
+
+from PIL import Image
+from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6.QtCore import Qt
+
+class Snipper(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+
+        self.setWindowTitle("TextShot")
+        self.setWindowFlags(
+            Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.Dialog
+        )
+        self.setWindowState(self.windowState() | Qt.WindowFullScreen)
+
+        self.screen = QtWidgets.QApplication.screenAt(QtGui.QCursor.pos()).grabWindow(0)
+        palette = QtGui.QPalette()
+        palette.setBrush(self.backgroundRole(), QtGui.QBrush(self.screen))
+        self.setPalette(palette)
+
+        QtWidgets.QApplication.setOverrideCursor(QtGui.QCursor(QtCore.Qt.CrossCursor))
+
+        self.start, self.end = QtCore.QPoint(), QtCore.QPoint()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Escape:
+            QtWidgets.QApplication.quit()
+
+        return super().keyPressEvent(event)
+
+    def paintEvent(self, event):
+        painter = QtGui.QPainter(self)
+        painter.setPen(Qt.NoPen)
+        painter.setBrush(QtGui.QColor(0, 0, 0, 100))
+        painter.drawRect(0, 0, self.width(), self.height())
+
+        if self.start == self.end:
+            return super().paintEvent(event)
+
+        painter.setPen(QtGui.QPen(QtGui.QColor(255, 0, 255), 3))
+        painter.setBrush(painter.background())
+        painter.drawRect(QtCore.QRect(self.start, self.end))
+        return super().paintEvent(event)
+
+    def mousePressEvent(self, event):
+        self.start = self.end = event.pos()
+        self.update()
+        return super().mousePressEvent(event)
+
+    def mouseMoveEvent(self, event):
+        self.end = event.pos()
+        self.update()
+        return super().mousePressEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        if self.start == self.end:
+            return super().mouseReleaseEvent(event)
+
+        self.hide()
+        QtWidgets.QApplication.processEvents()
+        shot = self.screen.copy(
+            min(self.start.x(), self.end.x()),
+            min(self.start.y(), self.end.y()),
+            abs(self.start.x() - self.end.x()),
+            abs(self.start.y() - self.end.y()),
+        )
+        processImage(shot)
+        QtWidgets.QApplication.quit()
+
+
+def processImage(img):
+    buffer = QtCore.QBuffer()
+    buffer.open(QtCore.QBuffer.ReadWrite)
+    img.save(buffer, "PNG")
+    pil_img = Image.open(io.BytesIO(buffer.data()))
+    img_name_str = 'grab_clipboard.png'
+    pil_img.save(img_name_str)
+    buffer.close()
+    cb = QtWidgets.QApplication.clipboard()
+    cb.clear()
+    cb.setImage(img_name_str)
+
+
+if __name__ == "__main__":
+    QtCore.QCoreApplication.setAttribute(Qt.AA_DisableHighDpiScaling)
+    app = QtWidgets.QApplication(sys.argv)
+    window = QtWidgets.QMainWindow()
+    snipper = Snipper(window)
+    snipper.show()
+    sys.exit(app.exec())
+
+# pyinstaller -D -w lib\\textshot.py
